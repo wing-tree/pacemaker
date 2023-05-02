d34318fd795a489f991f08cce0055949c5533f52
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-05-02 09:14:55 +0900

    Setup worker provider

--- app/src/main/java/wing/tree/pacemaker/Application.kt
+++ app/src/main/java/wing/tree/pacemaker/Application.kt
@@ -1,7 +1,17 @@
 package wing.tree.pacemaker
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class Application : Application()
+class Application : Application(), Configuration.Provider {
+    @Inject
+    lateinit var hiltWorkerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration() = Configuration.Builder()
+        .setWorkerFactory(hiltWorkerFactory)
+        .build()
+}
